afcccb5c751041cd01305633e196789b5f12fd61
Anand Jose <anand.jose@finly.io>
2020-10-29 01:24:47 +0530

    completed ui

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,75 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List todos = List();
+  String input = "";
+
   @override
+  void initState() {
+    super.initState();
+    todos.add("Item1");
+    todos.add("Item2");
+    todos.add("Item3");
+    todos.add("Add new");
+    todos.add("flutter aweosome");
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("mytodos"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("mytodos"),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                      title: Text("Add new Taskr"),
+                      content: TextField(
+                        onChanged: (String value) {
+                          input = value;
+                        },
+                      ),
+                      actions: <Widget>[
+                        FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                todos.add(input);
+                                Navigator.of(context, rootNavigator: true)
+                                    .pop('dialog');
+                              });
+                            },
+                            child: Text("Add"))
+                      ]);
+                },
+              );
+            },
+            child: Icon(Icons.add)),
+        body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key(todos[index]),
+              child: Card(
+                elevation: 4,
+                margin: EdgeInsets.all(8),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                child: ListTile(
+                  title: Text(todos[index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {
+                      setState(() {
+                        todos.removeAt(index);
+                      });
+                    },
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
